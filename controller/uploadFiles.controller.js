48ad9871a52ef9057de8858fbe8040cb04eb849a
Nikitakan <nikitakanwardev@gmail.com>
2022-07-22 17:58:31 +0530

    img path fixed

--- controller/uploadFiles.controller.js
+++ controller/uploadFiles.controller.js
@@ -2,7 +2,7 @@ const {sendResponse}=require("../helpers/requestHandler.helper")
 const {APP_URL} = require("../config/app.config")
 exports.uploadImage = async (req, res, next) => {
     try {
-        return sendResponse(res, true, 200, "Image uploaded",APP_URL+req.file.filename);
+        return sendResponse(res, true, 200, "Image uploaded",APP_URL+"/"+req.file.filename);
     } catch (error) {
         next(error);
     }
