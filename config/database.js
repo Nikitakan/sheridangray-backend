86585ca8f174ee6fdd989bdb61a6a2bfed05473e
Nikitakan <nikitakanwardev@gmail.com>
2022-08-05 18:28:51 +0530

    chnages

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/sheridangryDB")
+        const conn = await mongoose.connect(process.env.MONGO_LIVE_URL || "mongodb+srv://sheridangray:JSsf72SA27dzFui@cluster0.acn3fdt.mongodb.net/sheridangryDB?retryWrites=true&w=majority")
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
