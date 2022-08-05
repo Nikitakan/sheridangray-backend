e49d1d7365a0627dafb5f3c102c6f5c38fdda872
Nikitakan <nikitakanwardev@gmail.com>
2022-08-05 16:59:37 +0530

    changes

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = Object.freeze({
-    APP_PORT: process.env.PORT,
+    APP_PORT: process.env.PORT || 3000,
     APP_NAME: process.env.APP_NAME,
     APP_URL: process.env.APP_URL,
     APP_ENV: process.env.NODE_ENV,
